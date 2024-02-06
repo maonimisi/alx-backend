2acbfbebf96663e29605c1d690eea7aac38f0e05
Muhammed Abdulrasheed Onimisi <maonimisi>
2024-02-06 14:56:15 +0100

    Basic Flask App

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+  Setup a basic Flask app in 0-app.py. Create a single / route and an index.html template that simply outputs “Welcome to Holberton” as page title (<title>) and “Hello world” as header (<h1>).
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template('0-index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
