2bf953d053229fbaebb1dc4ee08b5c54c9f555e7
Muhammed Abdulrasheed Onimisi <maonimisi>
2024-02-06 15:06:19 +0100

    Basic Flask App

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,14 +1,23 @@
 #!/usr/bin/env python3
 """
-  Setup a basic Flask app in 0-app.py. Create a single / route and an index.html template that simply outputs “Welcome to Holberton” as page title (<title>) and “Hello world” as header (<h1>).
+  Setup a basic Flask app in 0-app.py. Create a single / route and an
+  index.html template that simply outputs “Welcome to Holberton” as page
+  title (<title>) and “Hello world” as header (<h1>).
 """
 from flask import Flask, render_template
+from os import getenv
+
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
+
+@app.route("/", strict_slashes=False)
+def index() -> str:
+    "This rounte render 0-index.html from the templates directory"
     return render_template('0-index.html')
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
